4399cfca66e0f60fb281e22cb665bcccc43a89b9
InuGans <102963507+Xenz-11@users.noreply.github.com>
2022-04-07 19:19:49 +0700

    Create puasa.sh

---
+++ puasa.sh
@@ -0,0 +1,37 @@
+# !/bin/bash/sh
+#===[ WARNA ]===#Bash
+me="\033[31m"
+hi="\033[32m"
+ku="\033[1;33m"
+bi="\033[34m"
+bm="\33[1;36m"
+pu="\033[37m"
+ht='\e[30;1m'
+un='\e[1;35m'
+tr='\e[30;2m' #transparan
+hr='\e[30;3m' #hitam miring
+hg='\e[30;4m' #hitam garis bawah
+hw='\e[30;7m' #hitam berwadah
+hgt='\e[30;9m' #hitam garis tengah
+kh='\e[1;40m'  #kotak hitam
+km='\e[1;41m'  #kotak merah
+khi='\e[1;42m'  #kotak hijau
+kk='\e[1;43m'  #kotak kuning
+kb='\e[1;44m'   #kotak biru
+kmm='\e[1;45m'  #kotak pink
+kc='\e[1;46m'   #kotak biru muda
+kp='\e[1;47m'   #kotak putih
+clear
+echo -e $hi""
+toilet -F border -f ASCII-Shadow.flf " SPAM WA "
+echo -e $pu""
+echo -e $km" Creation By : @Xenz-11                  "
+echo -e $km" Github      : https://github.com/Xenz-11"
+echo -e $km" Facebook    : Xenz Why                  "
+
+#================================#
+cd.module
+php spam.php
+#===============================#
+#                               #
+#################################
